977cde15f4c708246c18e4a8995d15645e4fa662
Leandra Tejedor <lmtejedor@gmail.com>
2013-09-26 19:03:21 -0400

    first commit

---
+++ src/testApp.cpp
@@ -0,0 +1,72 @@
+#include "testApp.h"
+
+//one line comment
+/*
+    multi line comment
+    go crazy wahhh
+*/
+
+float playerX;
+float playerY;
+
+//--------------------------------------------------------------
+void testApp::setup(){
+    playerX = 10;
+    playerY = 10;
+
+}
+
+//--------------------------------------------------------------
+void testApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+
+}
+
+//--------------------------------------------------------------
+void testApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void testApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void testApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
