1702f979b0d77eddae8781ad6cc54ff6ad8651d5
Leandra Tejedor <lmtejedor@gmail.com>
2013-09-26 20:58:30 -0400

    randomly generate dots

--- src/testApp.cpp
+++ src/testApp.cpp
@@ -10,7 +10,6 @@ float playerX;
 float playerY;
 float playerRadius;
 float playerSpeed;
-bool isHoldingObject;
 
 int score;
 float scoreX;
@@ -25,6 +24,8 @@ float goalRadius;
 const int numofObj = 10;
 float objectX[numofObj];
 float objectY[numofObj];
+float objectRadius;
+bool isHoldingObject[numofObj];
 
 //--------------------------------------------------------------
 void testApp::setup(){
@@ -32,12 +33,23 @@ void testApp::setup(){
     playerY = ofRandom(ofGetHeight());
     playerRadius = 30;
     playerSpeed = 20;
-    isHoldingObject = false;
+    objectRadius = 10;
+    
+    for (int i = 0; i < numofObj; i++) {
+        objectX[i] = ofRandom(ofGetWidth());
+        objectY[i] = ofRandom(ofGetHeight());
+    }
 
 }
 
 //--------------------------------------------------------------
 void testApp::update(){
+    
+    for (int i = 0; i< numofObj; i++) {
+        if (ofDist(objectX[i], objectY[i], playerX, playerY) < objectRadius + playerRadius) {
+            isHoldingObject[i] = true;
+        }
+    }
 
 
 }
@@ -50,15 +62,24 @@ void testApp::draw(){
     
     ofSetColor(120, 200, 255);
     ofCircle(playerX, playerY, playerRadius);
+    
+    ofSetColor(255, 151, 0);
+    
+    for (int i = 0; i < numofObj; i++) {
+        ofCircle(objectX[i], objectY[i], objectRadius);
+    }
 
 }
 //--------------------------------------------------------------
 void testApp::keyPressed(int key){
     if (key == OF_KEY_UP) {
         playerY -= playerSpeed;
-    }
-    if (key == OF_KEY_DOWN) {
+    } else if (key == OF_KEY_DOWN) {
         playerY += playerSpeed;
+    } else if (key == OF_KEY_LEFT) {
+        playerX -= playerSpeed;
+    } else if (key == OF_KEY_RIGHT) {
+        playerX += playerSpeed;
     }
 
 }
